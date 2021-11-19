d979110cad20909f0900208b01223b902c1beead
kuncy <kuncypiotr@gmail.com>
2021-11-19 23:49:52 +0100

    Added basic chair functionality

--- src/furnitures/chair.js
+++ src/furnitures/chair.js
@@ -1,16 +1,57 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
-export function createChair(scene, objects) {
+export function createChair(scene, objects, geometryDims) {
   while (scene.children.length > 0) {
     scene.remove(scene.children[0]);
   }
   const texture = new THREE.TextureLoader().load("https://i.imgur.com/xLJVKyJ.jpeg");
 
-  const geometry = new THREE.BoxGeometry(1, 1, 1);
-  const material = new THREE.MeshBasicMaterial({ map: texture });
+  var legPosX = geometryDims[0][0] / 2 - geometryDims[2][0] / 2;
+  var legPosY = (geometryDims[2][1] / 2 + geometryDims[0][1] / 2) * -1;
+  var legPosZ = geometryDims[0][2] / 2 - geometryDims[2][2] / 2;
 
-  let mesh = new THREE.Mesh(geometry, material);
+  var chairBackPosX = geometryDims[0][0] / 2 - geometryDims[1][0] / 2;
+  var chairBackPosY = geometryDims[1][1] / 2 + geometryDims[0][1] / 2;
+  var chairBackPosZ = (geometryDims[0][2] / 2 - geometryDims[1][2] / 2) * -1;
+
+  const geometryPos = [
+    [0, 0, 0],
+    [chairBackPosX, chairBackPosY, chairBackPosZ],
+    [legPosX, legPosY, legPosZ],
+    [legPosX * -1, legPosY, legPosZ],
+    [legPosX * -1, legPosY, legPosZ * -1],
+    [legPosX, legPosY, legPosZ * -1],
+  ];
+  for (let i = 0; i < geometryPos.length; i++) {
+    console.log(geometryDims[0][0] + " " + geometryDims[0][1] + " " + geometryDims[0][2]);
+    const material = new THREE.MeshBasicMaterial({ map: texture });
+    if (i == 0) {
+      var geometry = new THREE.BoxGeometry(geometryDims[0][0], geometryDims[0][1], geometryDims[0][2]);
+      var mesh = new THREE.Mesh(geometry, material);
+      mesh.position.x = geometryPos[0][0];
+      mesh.position.y = geometryPos[0][1];
+      mesh.position.z = geometryPos[0][2];
+    }
+    if (i == 1) {
+      var geometry = new THREE.BoxGeometry(geometryDims[1][0], geometryDims[1][1], geometryDims[1][2]);
+      var mesh = new THREE.Mesh(geometry, material);
+      mesh.position.x = geometryPos[1][0];
+      mesh.position.y = geometryPos[1][1];
+      mesh.position.z = geometryPos[1][2];
+    }
+    if (i >= 2) {
+      var geometry = new THREE.BoxGeometry(geometryDims[2][0], geometryDims[2][1], geometryDims[2][2]);
+      var mesh = new THREE.Mesh(geometry, material);
+      mesh.position.x = geometryPos[i][0];
+      mesh.position.y = geometryPos[i][1];
+      mesh.position.z = geometryPos[i][2];
+    }
+
+    scene.add(mesh);
+    objects.push(mesh);
+  }
+
   scene.add(mesh);
   objects.push(mesh);
 }
--- src/script.js
+++ src/script.js
@@ -18,8 +18,14 @@ let tableGeometryDims = [
   [0.1, 0.5, 0.1], // legs
 ];
 
+let chairGeometryDims = [
+  [0.75, 0.1, 0.75], // chairbase
+  [0.75, 1, 0.1], // chair back
+  [0.1, 0.75, 0.1], // legs
+];
+
 let changeBtn = document.createElement("button");
-changeBtn.innerHTML = "Accept changes";
+changeBtn.innerHTML = "Apply changes";
 changeBtn.id = "change-btn";
 changeBtn.addEventListener("click", updateDims);
 
@@ -42,37 +48,73 @@ function initTableDOM() {
     4 * 2 * (tableGeometryDims[1][0] * tableGeometryDims[1][2] + tableGeometryDims[1][0] * tableGeometryDims[1][1] + tableGeometryDims[1][0] * tableGeometryDims[1][1]);
   wholematerial = Math.round(wholematerial * 100) / 100;
   tableInfo.innerHTML = "<span id='area'>Area of tabletop:" + area + "m²</span><br/><span id='wholematerial'>Whole material:" + wholematerial + "m³</span>";
-  var inputContainer = document.createElement("div");
-
+  var inputTabletop = document.createElement("div");
+  var inputLegs = document.createElement("div");
   for (var i = 0; i < 3; i++) {
     switch (i) {
       case 0:
-        var label = "Length of tabletop";
+        var label = "Width of tabletop";
         break;
       case 1:
-        var label = "Thickness of tabletop";
+        var label = "Height of tabletop";
         break;
       case 2:
-        var label = "Height of tabletop";
+        var label = "Length of tabletop";
         break;
     }
     var inputDiv = document.createElement("div");
     var input = document.createElement("input");
     var unit = document.createElement("span");
+    var inputType = document.createElement("small");
+    inputType.innerHTML = label;
+    inputType.style.position = "absolute";
+    inputType.style.marginTop = "-24px";
     unit.innerHTML = "mm";
-    input.placeholder = label;
     input.type = "number";
     input.className = "tableDimSet dimSet";
-    input.id = "pos" + i;
+    input.id = "tableTopPos" + i;
     input.step = "1";
     input.min = "1";
     input.value = tableGeometryDims[0][i] * 1000;
+    inputDiv.appendChild(inputType);
     inputDiv.appendChild(input);
     inputDiv.appendChild(unit);
-
-    inputContainer.appendChild(inputDiv);
+    inputTabletop.appendChild(inputDiv);
   }
-  tableContainer.appendChild(inputContainer);
+  for (var i = 0; i < 3; i++) {
+    switch (i) {
+      case 0:
+        var label = "Width of legs";
+        break;
+      case 1:
+        var label = "Height of legs";
+        break;
+      case 2:
+        var label = "Length of legs";
+        break;
+    }
+    var inputDiv = document.createElement("div");
+    var input = document.createElement("input");
+    var unit = document.createElement("span");
+    var inputType = document.createElement("small");
+    inputType.innerHTML = label;
+    inputType.style.position = "absolute";
+    inputType.style.marginTop = "-24px";
+    unit.innerHTML = "mm";
+    input.type = "number";
+    input.className = "tableDimSet dimSet";
+    input.id = "tableLegsPos" + i;
+    input.step = "1";
+    input.min = "1";
+    input.value = tableGeometryDims[1][i] * 1000;
+    inputDiv.appendChild(inputType);
+    inputDiv.appendChild(input);
+    inputDiv.appendChild(unit);
+    inputLegs.appendChild(inputDiv);
+  }
+  tableContainer.appendChild(inputLegs);
+  tableContainer.appendChild(inputTabletop);
+
   tableContainer.appendChild(tableInfo);
   document.body.appendChild(tableContainer);
   document.body.appendChild(changeBtn);
@@ -86,26 +128,120 @@ function initChairDOM() {
   if (previousContainer.length != 0) {
     previousContainer[0].remove();
   }
+
+  var chairContainer = document.createElement("div");
+  chairContainer.className = "furnitureInfoContainer";
+  var chairInfo = document.createElement("div");
+  area = Math.round(chairGeometryDims[0][0] * chairGeometryDims[0][2] * 100) / 100;
+  wholematerial =
+    2 * (chairGeometryDims[0][0] * chairGeometryDims[0][2] + chairGeometryDims[0][0] * chairGeometryDims[0][1] + chairGeometryDims[0][0] * chairGeometryDims[0][1]) +
+    2 * (chairGeometryDims[1][0] * chairGeometryDims[1][2] + chairGeometryDims[1][0] * chairGeometryDims[1][1] + chairGeometryDims[1][0] * chairGeometryDims[1][1]) +
+    4 * 2 * (chairGeometryDims[2][0] * chairGeometryDims[2][2] + chairGeometryDims[2][0] * chairGeometryDims[2][1] + chairGeometryDims[2][0] * chairGeometryDims[2][1]);
+  wholematerial = Math.round(wholematerial * 100) / 100;
+  chairInfo.innerHTML = "<span id='area'>Area of tabletop:" + area + "m²</span><br/><span id='wholematerial'>Whole material:" + wholematerial + "m³</span>";
+  var inputChairBase = document.createElement("div");
+  var inputChairBack = document.createElement("div");
+  var inputLegs = document.createElement("div");
   for (var i = 0; i < 3; i++) {
     switch (i) {
       case 0:
-        var label = "Dimension X for chair";
+        var label = "Width of chair base";
         break;
       case 1:
-        var label = "Dimension Y for chair";
+        var label = "Height of chair base";
         break;
       case 2:
-        var label = "Dimension Z for chair";
+        var label = "Length of chair base";
         break;
     }
+    var inputDiv = document.createElement("div");
     var input = document.createElement("input");
-    input.placeholder = label;
+    var unit = document.createElement("span");
+    var inputType = document.createElement("small");
+    inputType.innerHTML = label;
+    inputType.style.position = "absolute";
+    inputType.style.marginTop = "-24px";
+    unit.innerHTML = "mm";
     input.type = "number";
     input.className = "chairDimSet dimSet";
-    input.id = "pos" + i;
-    document.body.appendChild(input);
-    document.body.appendChild(changeBtn);
+    input.id = "chairBasePos" + i;
+    input.step = "1";
+    input.min = "1";
+    input.value = chairGeometryDims[0][i] * 1000;
+    inputDiv.appendChild(inputType);
+    inputDiv.appendChild(input);
+    inputDiv.appendChild(unit);
+    inputChairBase.appendChild(inputDiv);
   }
+  for (var i = 0; i < 3; i++) {
+    switch (i) {
+      case 0:
+        var label = "Width of chair back";
+        break;
+      case 1:
+        var label = "Height of chair back";
+        break;
+      case 2:
+        var label = "Length of chair back";
+        break;
+    }
+    var inputDiv = document.createElement("div");
+    var input = document.createElement("input");
+    var unit = document.createElement("span");
+    var inputType = document.createElement("small");
+    inputType.innerHTML = label;
+    inputType.style.position = "absolute";
+    inputType.style.marginTop = "-24px";
+    unit.innerHTML = "mm";
+    input.type = "number";
+    input.className = "chairDimSet dimSet";
+    input.id = "chairBackPos" + i;
+    input.step = "1";
+    input.min = "1";
+    input.value = chairGeometryDims[1][i] * 1000;
+    inputDiv.appendChild(inputType);
+    inputDiv.appendChild(input);
+    inputDiv.appendChild(unit);
+    inputChairBack.appendChild(inputDiv);
+  }
+  for (var i = 0; i < 3; i++) {
+    switch (i) {
+      case 0:
+        var label = "Width of legs";
+        break;
+      case 1:
+        var label = "Height of legs";
+        break;
+      case 2:
+        var label = "Length of legs";
+        break;
+    }
+    var inputDiv = document.createElement("div");
+    var input = document.createElement("input");
+    var unit = document.createElement("span");
+    var inputType = document.createElement("small");
+    inputType.innerHTML = label;
+    inputType.style.position = "absolute";
+    inputType.style.marginTop = "-24px";
+    unit.innerHTML = "mm";
+    input.type = "number";
+    input.className = "chairDimSet dimSet";
+    input.id = "chairLegsPos" + i;
+    input.step = "1";
+    input.min = "1";
+    input.value = chairGeometryDims[2][i] * 1000;
+    inputDiv.appendChild(inputType);
+    inputDiv.appendChild(input);
+    inputDiv.appendChild(unit);
+    inputLegs.appendChild(inputDiv);
+  }
+  chairContainer.appendChild(inputLegs);
+  chairContainer.appendChild(inputChairBack);
+  chairContainer.appendChild(inputChairBase);
+
+  chairContainer.appendChild(chairInfo);
+  document.body.appendChild(chairContainer);
+  document.body.appendChild(changeBtn);
 }
 
 function onWindowResize() {
@@ -132,15 +268,17 @@ function updateMesh() {
   if (furniture == "chair") {
     currFurniture = "chair";
     initChairDOM();
-    createChair(scene, objects);
+    createChair(scene, objects, chairGeometryDims);
   }
 }
 
 function updateDims() {
-  console.log("dupa");
   if (currFurniture == "table") {
     for (var i = 0; i < 3; i++) {
-      tableGeometryDims[0][i] = document.getElementById("pos" + i).value / 1000;
+      tableGeometryDims[0][i] = document.getElementById("tableTopPos" + i).value / 1000;
+    }
+    for (var i = 0; i < 3; i++) {
+      tableGeometryDims[1][i] = document.getElementById("tableLegsPos" + i).value / 1000;
     }
     area = Math.round(tableGeometryDims[0][0] * tableGeometryDims[0][2] * 100) / 100;
     wholematerial =
@@ -150,6 +288,26 @@ function updateDims() {
     document.getElementById("area").innerHTML = "Area of tabletop:" + area + "m²";
     document.getElementById("wholematerial").innerHTML = "Whole material:" + wholematerial + "m³";
     createTable(scene, objects, tableGeometryDims);
+  }
+  if (currFurniture == "chair") {
+    for (var i = 0; i < 3; i++) {
+      chairGeometryDims[0][i] = document.getElementById("chairBasePos" + i).value / 1000;
+    }
+    for (var i = 0; i < 3; i++) {
+      chairGeometryDims[1][i] = document.getElementById("chairBackPos" + i).value / 1000;
+    }
+    for (var i = 0; i < 3; i++) {
+      chairGeometryDims[2][i] = document.getElementById("chairLegsPos" + i).value / 1000;
+    }
+    area = Math.round(chairGeometryDims[0][0] * chairGeometryDims[0][2] * 100) / 100;
+    wholematerial =
+      2 * (chairGeometryDims[0][0] * chairGeometryDims[0][2] + chairGeometryDims[0][0] * chairGeometryDims[0][1] + chairGeometryDims[0][0] * chairGeometryDims[0][1]) +
+      2 * (chairGeometryDims[1][0] * chairGeometryDims[1][2] + chairGeometryDims[1][0] * chairGeometryDims[1][1] + chairGeometryDims[1][0] * chairGeometryDims[1][1]) +
+      4 * 2 * (chairGeometryDims[2][0] * chairGeometryDims[2][2] + chairGeometryDims[2][0] * chairGeometryDims[2][1] + chairGeometryDims[2][0] * chairGeometryDims[2][1]);
+    wholematerial = Math.round(wholematerial * 100) / 100;
+    document.getElementById("area").innerHTML = "Area of chairbase:" + area + "m²";
+    document.getElementById("wholematerial").innerHTML = "Whole material:" + wholematerial + "m³";
+    createChair(scene, objects, chairGeometryDims);
   }
 }
 
